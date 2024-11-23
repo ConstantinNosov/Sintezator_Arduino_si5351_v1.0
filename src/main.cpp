11d435821c084ee042fcb10e7429313bbbffd95d
ConstantinNosov <constantinnosov258@gmail.com>
2024-11-24 00:49:54 +0600

    compil_ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,47 @@
-/*
-  Вся эта программа изначально написана NT7S и SQ9NJE.
-  Оригинальный код, немного подправленный мною, под мои нужды.
-  Ознакомится с оригиналом можно здесь
-  http://nt7s.com/
-  http://sq9nje.pl/
-  http://ak2b.blogspot.com/
-*/
-
 #include <Rotary.h>
 #include <si5351.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
-//Константы
-#define F_MIN        3500000L               // Нижний предел частоты
-#define F_MAX        14350000L              // Верхний предел частоты
+#define F_MIN        3500000L 
+#define F_MAX        14350000L              
 #define OLED_RESET   4
-#define ENCODER_A    3                      // Encoder pin A
-#define ENCODER_B    2                      // Encoder pin B
-#define ENCODER_BTN  11
+#define ENCODER_A    3                      
+#define ENCODER_B    2   
+#define ENCODER_BTN  11          
 
+LiquidCrystal_I2C lcd(0x27,16,2); 
+Si5351 si5351; 
+Rotary r = Rotary(ENCODER_A, ENCODER_B); 
 
-// Переменные нужные для кнопок PRE/ATT
-int regim = 1; // для кнопки
-int flag = 0; // для кнопки
+// Переменные кнопок PRE/ATT
+int regim = 1;
+int flag = 0; 
 
-LiquidCrystal_I2C lcd(0x27,16,2); // set the LiquidCrystal_I2C  address to 0x27 for a 16 chars and 2 line display  
-
-Si5351 si5351;
-Rotary r = Rotary(ENCODER_A, ENCODER_B);
-
-// Для кварцевых резонаторов со значением 8867 МГц.
+// Для кварцевых резонаторов полосового фильтра со значением 8867 МГц.
+//Эти USB/LSB частоты добавляется или вычитается из частоты VFO в "void loop()"
 volatile uint32_t LSB = 50000000ULL; //частота ОГ(гетеродина) для "нижней" боковой. Настр. на ниж. скат КФ.
 volatile uint32_t USB = 50300000ULL; //частота ОГ(гетеродина) для "верхней" боковой. Настр. на вверхн. скат КФ.
-volatile uint32_t bfo = 50000000ULL; //стартовать с "верхней" ...
-//Эти USB/LSB частоты добавляется или вычитается из частоты VFO в "void loop()"
-//В этом примере если начальная частота будет 14.20000 плюс 9.001500 то на выходе clk0 = 23.2015Mhz
-volatile uint32_t vfo = 710000000ULL / SI5351_FREQ_MULT; //стартовая частота при запуске синтезатора.
-volatile uint32_t radix = 100000;  // Шаг перестройки по умолчанию при старте = 100 кГц
+volatile uint32_t bfo = 50000000ULL; // частота второго гетеродина, при старте вкл.верхняя боковая
+volatile uint32_t vfo = 710000000ULL / SI5351_FREQ_MULT;  //стартовая частота при запуске синтезатора.
+volatile uint32_t step = 100000;  // Шаг перестройки по умолчанию при старте = 100 кГц
 boolean changed_f = 0;
-String tbfo = "";
-
+String lsb_usb = "";
 
 //------------------ Установка дополнительных функций здесь  ---------------------------
 //Удалить коммент (//) для применения нужного варианта. Задействовать только одно.
-#define IF_Offset // Показание на ЖКИ плюс(минус) на значение ПЧ
+#define IF_Offset// Показание на ЖКИ плюс(минус) на значение ПЧ
 //#define Direct_conversion // чатота на выходе как на ЖКИ. Прямой выход. Генератор.
 //#define FreqX4  // частота на выходе, умноженная на четыре ...
 //#define FreqX2  // частота на выходе, умноженная на два ...
 //---------------------------------------------------------------------------------------
 
-/**************************************/
-/* Interrupt service routine for      */
-/* encoder frequency change           */
-/**************************************/
-ISR(PCINT2_vect) {
-  unsigned char result = r.process();
-  if (result == DIR_CW)
-    set_frequency(1);
-  else if (result == DIR_CCW)
-    set_frequency(-1);
-}
-
-/**************************************/
-/* Change the frequency               */
-/* dir = 1    Increment               */
-/* dir = -1   Decrement               */
-/**************************************/
+// Функция установки частоты
 void set_frequency(short dir)
 {
   if (dir == 1)
-    vfo += radix;
+    vfo += step;
   if (dir == -1)
-    vfo -= radix;
+    vfo -= step;
 
   if (vfo > F_MAX)
     vfo = F_MAX;
@@ -82,9 +51,17 @@ void set_frequency(short dir)
   changed_f = 1;
 }
 
-/**************************************/
-/* Read the button with debouncing    */
-/**************************************/
+// Установка частоты по сигналу энкодера через прерывание
+ISR(PCINT2_vect) 
+{
+  unsigned char result = r.process();
+  if (result == DIR_CW)
+    set_frequency(1);
+  else if (result == DIR_CCW)
+    set_frequency(-1);
+}
+
+//Читаем кнопку энкодера
 boolean get_button()
 {
   if (!digitalRead(ENCODER_BTN))
@@ -99,46 +76,41 @@ boolean get_button()
   return 0;
 }
 
-/**************************************/
-/* Displays the frequency             */
-/**************************************/
+
+ //Функция вывода значения частоты на дисплей
 void display_frequency()
 {
-  uint16_t f, g;
-
+  uint16_t frequency;
   lcd.setCursor(3, 0);
-  f = vfo / 1000000; 	//variable is now vfo instead of 'frequency'
-  if (f < 10)
+  frequency = vfo / 1000000;
     lcd.print(' ');
-  lcd.print(f);
+  lcd.print(frequency);
   lcd.print('.');
-  f = (vfo % 1000000) / 1000;
-  if (f < 100)
+  frequency = (vfo % 1000000) / 1000;
+  if (frequency < 100)
     lcd.print('0');
-  if (f < 10)
+  if (frequency < 10)
     lcd.print('0');
-  lcd.print(f);
+  lcd.print(frequency);
   lcd.print('.');
-  f = vfo % 1000;
-  if (f < 100)
+  frequency = vfo % 1000;
+  if (frequency < 100)
     lcd.print('0');
-  if (f < 10)
+  if (frequency < 10)
     lcd.print('0');
-  lcd.print(f);
+  lcd.print(frequency);
   lcd.print("Hz ");
   lcd.setCursor(0, 1);
-  lcd.print(tbfo);
+  lcd.print(lsb_usb);
   //Serial.println(vfo + bfo);
   //Serial.println(tbfo);
 }
 
-/**************************************/
-/* Отображаем шаг изменения частоты */
-/**************************************/
-void display_radix()
+//Функция отображения шага частоты
+void display_step()
 {
   lcd.setCursor(9, 1);
-  switch (radix)
+  switch (step)
   {
     case 1:
       lcd.print("    1");
@@ -167,70 +139,55 @@ void display_radix()
   lcd.print("Hz");
 }
 
+
 void setup() {
-lcd.init();
-lcd.backlight();
 
-PCICR |= (1 << PCIE2);
-PCMSK2 |= (1 << PCINT18) | (1 << PCINT19);
-sei();
-
-
-  // начальное сообщение
-  //   LiquidCrystal_I2C .print("SINTIZER Si5351"); // текст на экране - "  HELLO !!! "
-  //   delay(90000); // Время на прочтение начального сообщения 3 сек
-
+  lcd.init();
+  lcd.backlight();
+  PCICR |= (1 << PCIE2);
+  PCMSK2 |= (1 << PCINT18) | (1 << PCINT19);
+  sei();
   Serial.begin(19200);
-  lcd .begin(16, 2);          // Initialize and clear the LiquidCrystal_I2C 
+  lcd .begin(16, 2);   
   lcd .clear();
   Wire.begin();
-
-  si5351.set_correction(71000000); //**mine. There is a calibration sketch in File/Examples/si5351Arduino
-  //where you can determine the correction by using the serial monitor.
-  //ввести калибровочное знечение в строчке si5351.set_correction(хххххххх).
-  //Нужно вычислить запустив "si5351calibration" в папке с примерами библиотеки si5351.
-
-  //initialize the Si5351
-  si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0); //If you're using a 27Mhz crystal, put in 25000000 instead of 0
-  // 0 is the default crystal frequency of 25Mhz.
-
+  int32_t correction = 10000; // Значение коррекции частоты синтезатора
+  si5351.set_correction(correction, SI5351_PLL_INPUT_XO);
+  si5351.init(SI5351_CRYSTAL_LOAD_8PF, 25000000, 0);  // 8pF для кристалла, 25 МГц частота, 0 коррекция
   si5351.set_pll(SI5351_PLL_FIXED, SI5351_PLLA);
-  // Set CLK0 to output the starting "vfo" frequency as set above by vfo = ?
 
+  // Установка выходной частоты в соответствии с доп.настройами (см.выше)
 #ifdef IF_Offset
-  si5351.set_freq((vfo * SI5351_FREQ_MULT) + bfo, SI5351_PLL_FIXED, SI5351_CLK0);
-  volatile uint32_t vfoT = (vfo * SI5351_FREQ_MULT) + bfo;
-  tbfo = "USB";
+  si5351.set_freq((vfo * SI5351_FREQ_MULT) + bfo, SI5351_CLK0);
+  //volatile uint32_t vfoT = (vfo * SI5351_FREQ_MULT) + bfo;
+  lsb_usb = "USB";
   // Set CLK2 to output bfo frequency
-  si5351.set_freq( bfo, 0, SI5351_CLK2);
+  si5351.set_freq( bfo,SI5351_CLK2);
   //si5351.drive_strength(SI5351_CLK0,SI5351_DRIVE_2MA); //you can set this to 2MA, 4MA, 6MA or 8MA
   //si5351.drive_strength(SI5351_CLK1,SI5351_DRIVE_2MA); //be careful though - measure into 50ohms
   //si5351.drive_strength(SI5351_CLK2,SI5351_DRIVE_2MA); //
 #endif
 
 #ifdef Direct_conversion
-  si5351.set_freq((vfo * SI5351_FREQ_MULT), SI5351_PLL_FIXED, SI5351_CLK0);
+  si5351.set_freq((vfo * SI5351_FREQ_MULT),SI5351_CLK0);
 #endif
 
 #ifdef FreqX4
-  si5351.set_freq((vfo * SI5351_FREQ_MULT) * 4, SI5351_PLL_FIXED, SI5351_CLK0);
+  si5351.set_freq((vfo * SI5351_FREQ_MULT) * 4,SI5351_CLK0);
 #endif
 
 #ifdef FreqX2
-  si5351.set_freq((vfo * SI5351_FREQ_MULT) * 2, SI5351_PLL_FIXED, SI5351_CLK0);
+  si5351.set_freq((vfo * SI5351_FREQ_MULT) * 2, SI5351_CLK0);
 #endif
 
   pinMode(ENCODER_BTN, INPUT_PULLUP);
   PCICR |= (1 << PCIE2);           // Enable pin change interrupt for the encoder
   PCMSK2 |= (1 << PCINT18) | (1 << PCINT19);
   sei();
-  display_frequency();  // Update the display
-  display_radix();
-
-  // Порты, пины для управл. напр. УВЧ и АТТ
+  display_frequency(); 
+  display_step();
   pinMode(12, OUTPUT); // для кнопки УВЧ = A2
   pinMode(13, OUTPUT); //  для кнопки АТТ = A1
-
   // Порты, пины управл. напр. для дешифр. CD4028
   pinMode(14, OUTPUT); // b0 для A
   pinMode(15, OUTPUT); // b1 для B
@@ -238,51 +195,49 @@ sei();
   pinMode(17, OUTPUT); // b3 для C
 
 }
-
-
+//ГЛАВНЫЙ ЦИКЛ
 void loop(){
+
 int keypressed;
 
-  // Update the display if the frequency has been changed
+  // Обновление дисплея если частота изменена
 if (changed_f)
   {
     display_frequency();
 
 #ifdef IF_Offset
-    si5351.set_freq((vfo * SI5351_FREQ_MULT) + bfo, SI5351_PLL_FIXED, SI5351_CLK0);
-    // Вы также можете вычесть BFO, чтобы удовлетворить ваши потребности
-    //si5351.set_freq((vfo * SI5351_FREQ_MULT) - bfo  , SI5351_PLL_FIXED, SI5351_CLK0);
+    si5351.set_freq((vfo * SI5351_FREQ_MULT) + bfo, SI5351_CLK0);
 
-    if (vfo >= 10000000ULL & tbfo != "USB")
+    if (vfo >= 10000000ULL & lsb_usb != "USB")
     {
       bfo = USB;
-      tbfo = "USB";
-      si5351.set_freq( bfo, 0, SI5351_CLK2);
+      lsb_usb = "USB";
+      si5351.set_freq( bfo, SI5351_CLK2);
       Serial.println("We've switched from LSB to USB");
     }
-    else if (vfo < 10000000ULL & tbfo != "LSB")
+    else if (vfo < 10000000ULL & lsb_usb != "LSB")
     {
       bfo = LSB;
-      tbfo = "LSB";
-      si5351.set_freq( bfo, 0, SI5351_CLK2);
+      lsb_usb = "LSB";
+      si5351.set_freq( bfo,SI5351_CLK2);
       Serial.println("We've switched from USB to LSB");
     }
 
 #endif
 
 #ifdef Direct_conversion
-    si5351.set_freq((vfo * SI5351_FREQ_MULT), SI5351_PLL_FIXED, SI5351_CLK0);
-    tbfo = "";
+    si5351.set_freq((vfo * SI5351_FREQ_MULT), SI5351_CLK0);
+    lsb_usb = "";
 #endif
 
 #ifdef FreqX4
-    si5351.set_freq((vfo * SI5351_FREQ_MULT) * 4, SI5351_PLL_FIXED, SI5351_CLK0);
-    tbfo = "";
+    si5351.set_freq((vfo * SI5351_FREQ_MULT) * 4, SI5351_CLK0);
+    lsb_usb = "";
 #endif
 
 #ifdef FreqX2
-    si5351.set_freq((vfo * SI5351_FREQ_MULT) * 2, SI5351_PLL_FIXED, SI5351_CLK0);
-    tbfo = "";
+    si5351.set_freq((vfo * SI5351_FREQ_MULT) * 2, SI5351_CLK0);
+    lsb_usb = "";
 #endif
 
     changed_f = 0;
@@ -290,20 +245,18 @@ if (changed_f)
 
   // Кнопки УВЧ и АТТ ---------------------------------
   {
-    if (digitalRead(4) == HIGH && flag == 0) //если кнопка нажата
-      // и перемення flag равна 0 , то ...
-    {
-      regim++;
-      flag = 1;
-
+    if (digitalRead(4) == HIGH && flag == 0) 
+      {
+        regim++;
+        flag = 1;
       //это нужно для того что бы с каждым нажатием кнопки
       //происходило только одно действие
       // плюс защита от "дребезга"  100%
 
       if (regim > 4) //ограничим количество режимов
-      {
-        regim = 1; //так как мы используем только одну кнопку,
-        // то переключать режимы будем циклично
+        {
+          regim = 1; //так как мы используем только одну кнопку,
+          // то переключать режимы будем циклично
       }
 
     }
@@ -317,8 +270,8 @@ if (changed_f)
     {
       digitalWrite(12, LOW); // на пине нулевой уровень
       digitalWrite(13, LOW);
-      LiquidCrystal_I2C .setCursor(5, 1);
-      LiquidCrystal_I2C .print("   "); //  "пустое место"
+      lcd.setCursor(5, 1);
+      lcd.print("   "); //  "пустое место"
 
       //здесь может быть любое ваше действие
     }
@@ -326,38 +279,31 @@ if (changed_f)
     {
       digitalWrite(12, LOW); //включает PRE
       digitalWrite(13, HIGH);
-      LiquidCrystal_I2C .setCursor(5, 1); // место на экране для PRE
-      LiquidCrystal_I2C .print("PRE");
+      lcd.setCursor(5, 1); // место на экране для PRE
+      lcd.print("PRE");
 
       //здесь может быть любое ваше действие
     }
 
     if (regim == 3) //третий режим - очистка экрана
-
     {
       digitalWrite(12, LOW); //
       digitalWrite(13, LOW);
-      LiquidCrystal_I2C .setCursor(5, 1); // место текста на экране
-      LiquidCrystal_I2C .print("   ");    //  "пустое место"
+      lcd.setCursor(5, 1); // место текста на экране
+      lcd.print("   ");    //  "пустое место"
 
       //здесь может быть любое ваше действие
     }
-
-
     if (regim == 4) //третий режим - вкл. АТТ
     {
       digitalWrite(12, HIGH); //включает АТТ
       digitalWrite(13, LOW);
-      LiquidCrystal_I2C .setCursor(5, 1); // место на экране для АТТ
-      LiquidCrystal_I2C .print("ATT");
+      lcd.setCursor(5, 1); // место на экране для АТТ
+      lcd.print("ATT");
     }
     //  --------------------------------
     //  для кнопки РТТ -----------------
-
-
-
     //  ---------------------------------
-
     // Для управления CD4028
     //---A0-A1-A2-A3 ---pin, porn Arduino Pro Mini
     //---b0-b1-b2-b3----band
@@ -449,102 +395,100 @@ if (changed_f)
     // 160-метровый (1,81 - 2 МГц)
     if (vfo >= 1810000ULL && vfo <= 2000000ULL)
     {
-      LiquidCrystal_I2C .setCursor(0, 1);
-      LiquidCrystal_I2C .print("160m");
+      lcd.setCursor(0, 1);
+      lcd.print("160m");
     }
-
     else
       // 80-метровый (3,5 - 3,8 МГц)
       if (vfo >= 3500000ULL && vfo <= 3800000ULL)
       {
-        LiquidCrystal_I2C .setCursor(0, 1);
-        LiquidCrystal_I2C .print("80m ");
+        lcd.setCursor(0, 1);
+        lcd.print("80m ");
       }
       else
         // 40-метровый (7 - 7,2 МГц)
         if (vfo >= 7000000ULL && vfo <= 7200000ULL)
         {
-          LiquidCrystal_I2C .setCursor(0, 1);
-          LiquidCrystal_I2C .print("40m ");
+          lcd.setCursor(0, 1);
+          lcd.print("40m ");
         }
         else
           // 30-метровый (только телеграф 10,1 - 10,15 МГц)
           if (vfo >= 10100000ULL && vfo <= 10150000ULL)
           {
-            LiquidCrystal_I2C .setCursor(0, 1);
-            LiquidCrystal_I2C .print("30m ");
+            lcd.setCursor(0, 1);
+            lcd.print("30m ");
           }
           else
             // 20-метровый (14 - 14,35 МГц)
             if (vfo >= 14000000ULL && vfo <= 14350000ULL)
             {
-              LiquidCrystal_I2C .setCursor(0, 1);
-              LiquidCrystal_I2C .print("20m ");
+              lcd.setCursor(0, 1);
+              lcd.print("20m ");
             }
             else
               // 17-метровый (18,068 - 18,168 МГц)
               if (vfo >= 18068000ULL && vfo <= 18168000ULL)
               {
-                LiquidCrystal_I2C .setCursor(0, 1);
-                LiquidCrystal_I2C .print("17m ");
+                lcd.setCursor(0, 1);
+                lcd.print("17m ");
               }
               else
                 // 15-метровый (21 - 21,45 МГц)
                 if (vfo >= 21000000ULL && vfo <= 21450000ULL)
                 {
-                  LiquidCrystal_I2C .setCursor(0, 1);
-                  LiquidCrystal_I2C .print("15m ");
+                  lcd.setCursor(0, 1);
+                  lcd.print("15m ");
                 }
                 else
                   // 12-метровый (24,89 - 25,14 МГц)
                   if (vfo >= 24890000ULL && vfo <= 25140000ULL)
                   {
-                    LiquidCrystal_I2C .setCursor(0, 1);
-                    LiquidCrystal_I2C .print("12m ");
+                    lcd.setCursor(0, 1);
+                    lcd.print("12m ");
                   }
                   else
                     // 10-метровый (28 - 29,7 МГц)
                     if (vfo >= 28000000ULL && vfo <= 29700000ULL)
                     {
-                      LiquidCrystal_I2C .setCursor(0, 1);
-                      LiquidCrystal_I2C .print("10m ");
+                      lcd.setCursor(0, 1);
+                      lcd.print("10m ");
                     }
                     else
                       // Если за границей любительских - очистка экрана
                     {
-                      LiquidCrystal_I2C .setCursor(0, 1);
-                      LiquidCrystal_I2C .print("    ");
+                      lcd.setCursor(0, 1);
+                      lcd.print("    ");
                     }
 
-    //-----------------------------------
-    // Button press changes the frequency change step for 1 Hz steps
+    // Нажатие кнопки изменяет шаг изменения частоты
     if (get_button())
     {
-      switch (radix)
+      switch (step)
       {
         case 1:
-          radix = 10;
+          step = 10;
           break;
         case 10:
-          radix = 100;
+          step = 100;
           break;
         case 100:
-          radix = 1000;
+          step = 1000;
           break;
         case 1000:
-          radix = 10000;
+          step = 10000;
           break;
         case 10000:
-          radix = 100000;
+          step = 100000;
           break;
         case 100000:
-          radix = 1000000;
+          step = 1000000;
           break;
         case 1000000:
-          radix = 1;
+          step = 1;
           break;
       }
-      display_radix();
+      display_step();
     }
 
 
